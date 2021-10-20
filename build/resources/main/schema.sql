2281626d3388ccb33eb1fec6ba0a84655f2ed5df
zb226 <Snail Mail>
2021-10-21 00:30:57 +0200

    removed build folder

--- build/resources/main/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE outlet (
-    outlet_id VARCHAR(255) PRIMARY KEY
-);
-
-CREATE TABLE outlet_offeredservice (
-    id VARCHAR(255) PRIMARY KEY,
-    outlet_id VARCHAR(255) NOT NULL,
-    FOREIGN KEY (outlet_id) REFERENCES outlet(outlet_id)
-);
